3828838244bab84df13ec0f90e816ba6b28a55f5
sepee <sepee67@gmail.com>
2024-04-18 16:30:49 +0100

    Refactored code and improved interaction

--- app.js
+++ app.js
@@ -1,3 +1,75 @@
+
+var xpos  = 0;
+var ypos = 0;
+
+var xdelta = 0;
+var xdelta = 0;
+
+var angle_a = 0;
+var angle_b = 0;
+var angle_c = 0;
+var angle_d = 0;
+
+var sensitivity = 0.01;
+
+// dragging controls
+OnDragCanvas = function(event) {
+
+
+	xpos = event.pageX;
+	ypos = event.pageY;
+	
+  // centers the ball at (pageX, pageY) coordinates
+  function moveAt(pageX, pageY) {
+	  xdelta = pageX - xpos;
+	  ydelta = pageY - ypos;
+    xpos = pageX;
+    ypos = pageY;
+	
+	angle_a += sensitivity * xdelta;
+	angle_b += sensitivity * ydelta;
+  }
+
+  // move our absolutely positioned ball under the pointer
+  moveAt(event.pageX, event.pageY);
+
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+
+  // (2) move the ball on mousemove
+  document.addEventListener('mousemove', onMouseMove);
+
+  // (3) drop the ball, remove unneeded handlers
+  document.onmouseup = function() {
+    document.removeEventListener('mousemove', onMouseMove);
+    document.onmouseup = null;
+  };
+
+};
+
+
+
+
+var aspectRatio;
+
+function InitializeWebGLEnvironment(canvas)
+{
+	// Get A WebGL context
+	var gl = canvas.getContext("webgl");
+	
+	if (!gl) {
+	return;
+	}
+	
+	aspectRatio = canvas.width / canvas.height;
+		
+	// Tell WebGL how to convert from clip space to pixels
+	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+	return gl;
+}
+
 function createShader(gl, type, source) {
   var shader = gl.createShader(type);
   gl.shaderSource(shader, source);
@@ -9,6 +81,33 @@ function createShader(gl, type, source) {
 
   console.log(gl.getShaderInfoLog(shader));
   gl.deleteShader(shader);
+}
+
+var programFuncEval;
+var programDirect;
+
+function createShaderPrograms(gl)
+{
+	// Get the strings for our GLSL shaders
+	var funcEvalVertexShaderSource = document.querySelector("#func-eval-vertex-shader").text;
+	var directVertexShaderSource = document.querySelector("#direct-vertex-shader").text;
+	
+	var fragmentShaderSource = document.querySelector("#fragment-shader").text;
+
+	var userFunctionInput = document.getElementById("function-input").value;
+
+	if(userFunctionInput != "") 
+		funcEvalVertexShaderSource = funcEvalVertexShaderSource.replace("//USERDEFINED", "return " + userFunctionInput + ";")
+
+	// create GLSL shaders, upload the GLSL source, compile the shaders
+	var func_eval_vs = createShader(gl, gl.VERTEX_SHADER, funcEvalVertexShaderSource);
+	var direct_vs = createShader(gl, gl.VERTEX_SHADER, directVertexShaderSource);
+
+	var fs = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+	
+	// Link the two shaders into a program
+	programFuncEval = createProgram(gl, func_eval_vs, fs);
+	programDirect = createProgram(gl , direct_vs, fs);
 }
 
 function createProgram(gl, vertexShader, fragmentShader) {
@@ -25,111 +124,57 @@ function createProgram(gl, vertexShader, fragmentShader) {
   gl.deleteProgram(program);
 }
 
+var domainToScreenMatrix;
+var rangeToScreenMatrix
 
-	var t = 0;
-	var gridSize = 101;
-	var domSize = 5;
-	var scalingFactor = 1/domSize;
+var t = 0;
+var gridSize = 101;
+var domSize = 5;
+var scalingFactor = 1/domSize;
 
+// Generate Grid Mesh
+var gridMesh = Array();
+for (let xGrid = 0; xGrid < gridSize + 1; xGrid++) { 
+	for (let yGrid = 0; yGrid < gridSize + 1; yGrid++) 
+	{ 
+		var x = (xGrid / gridSize - 0.5) * domSize;
+		var y = (yGrid / gridSize - 0.5) * domSize;
+
+		if(xGrid < gridSize)
+		{
+			gridMesh.push(x);
+			gridMesh.push(y);	
+			gridMesh.push(x + 1.0 / gridSize * domSize);
+			gridMesh.push(y);
+		}
+
+		if(yGrid < gridSize)
+		{
+			gridMesh.push(x);
+			gridMesh.push(y);	
+			gridMesh.push(x);
+			gridMesh.push(y + 1.0 / gridSize* domSize);
+		}
+	}
+}
 
 function main() {
-	// Get A WebGL context
+	
 	var canvas = document.querySelector("#c");
-	var gl = canvas.getContext("webgl");
-	if (!gl) {
-	return;
-	}
-
-	// Get the strings for our GLSL shaders
-	var vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
-	var fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
-
-	var userFunctionInput = document.getElementById("function-input").value;
-
-	if(userFunctionInput != "") 
-		vertexShaderSource = vertexShaderSource.replace("//USERDEFINED", "return " + userFunctionInput + ";")
-
-	// create GLSL shaders, upload the GLSL source, compile the shaders
-	var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-	var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-
-	// Link the two shaders into a program
-	var program = createProgram(gl, vertexShader, fragmentShader);
-
-	// look up where the vertex data needs to go.
-	var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-
-	// Create a buffer and put three 2d clip space points in it
-	var positionBuffer = gl.createBuffer();
-
-	// Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
-	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
+	var gl = InitializeWebGLEnvironment(canvas);
+		
 	// Set background colour
-	gl.clearColor(1.0,1.0,1.0, 1.0);
-
-
-	// Tell WebGL how to convert from clip space to pixels
-	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-	// Tell it to use our program (pair of shaders)
-	gl.useProgram(program);
-
-	// Turn on the attribute
-	gl.enableVertexAttribArray(positionAttributeLocation);
+	gl.clearColor(0.5, 0.5, 0.5, 1.0);
 
 	gl.enable(gl.BLEND);
 	gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
 
-	// Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-	var size = 2;          // 2 components per iteration
-	var type = gl.FLOAT;   // the data is 32bit floats
-	var normalize = false; // don't normalize the data
-	var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-	var offset = 0;        // start at the beginning of the buffer
-	gl.vertexAttribPointer(
-	  positionAttributeLocation, size, type, normalize, stride, offset);
-	var offset = 0;
-
-    var domainMatLoc = gl.getUniformLocation(program, "u_dom_mat");
-	var rangeMatLoc = gl.getUniformLocation(program, "u_ran_mat");
-	var scalingFactorLoc = gl.getUniformLocation(program, "u_scaling_factor");
-
-
-	var primitiveType = gl.LINES;
-
-	var aspectRatio = canvas.width / canvas.height
-
-	gl.uniform1f(scalingFactorLoc, scalingFactor);
-
-	var positions = Array();
-
-	for (let xGrid = 0; xGrid < gridSize + 1; xGrid++) { 
-		for (let yGrid = 0; yGrid < gridSize + 1; yGrid++) 
-		{ 
-			var x = (xGrid / gridSize - 0.5) * domSize;
-			var y = (yGrid / gridSize - 0.5) * domSize;
-
-			if(xGrid < gridSize)
-			{
-				positions.push(x);
-				positions.push(y);	
-				positions.push(x + 1.0 / gridSize * domSize);
-				positions.push(y);
-			}
-
-			if(yGrid < gridSize)
-			{
-				positions.push(x);
-				positions.push(y);	
-				positions.push(x);
-				positions.push(y + 1.0 / gridSize* domSize);
-			}
-		}
-	}
-
-	var count = positions.length / size;
-
+	createShaderPrograms(gl);
+	
+	var gridPositionBuffer = gl.createBuffer();	// Create a buffer and put three 2d clip space points in it
+	gl.bindBuffer(gl.ARRAY_BUFFER, gridPositionBuffer);	// Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(gridMesh), gl.DYNAMIC_DRAW);
+	
 	drawFrame();
 
 function drawFrame()
@@ -137,36 +182,64 @@ function drawFrame()
 	gl.clear(gl.COLOR_BUFFER_BIT);
 
 	t += 0.01;
-
-	var theta = t;
-	var phi = 0.1 * t;
-
-	var domainToScreenMatrix = [
-		Math.cos(0), Math.sin(0) * aspectRatio, 0, 0,
-		Math.sin(theta), -Math.cos(theta) * aspectRatio, 0, 0,
-		0, 0, 1, 0,
-		0, 0, 0, 1
-	];
-
-	var rangeToScreenMatrix = [
-		Math.cos(phi), Math.sin(phi) * aspectRatio, 0, 0,
-		Math.sin(0), -Math.cos(0) * aspectRatio, 0, 0,
-		0, 0, 1, 0,
-		0, 0, 0, 1
-	];
-
-	gl.uniformMatrix4fv(domainMatLoc, false, domainToScreenMatrix);
-	gl.uniformMatrix4fv(rangeMatLoc, false, rangeToScreenMatrix);
-
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-	// Bind the position buffer.
-	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-	// draw
 	
-	gl.drawArrays(primitiveType, offset, count);
+	//angle_a = document.getElementById("angle-a").value;
+	//angle_b = document.getElementById("angle-b").value;
+	angle_c = document.getElementById("angle-c").value;
+	angle_d = document.getElementById("angle-d").value;
+	
+	var domain_scale = document.getElementById("domain-scale").value * scalingFactor;
+	var range_scale = document.getElementById("range-scale").value * scalingFactor;
+	
+	domainToScreenMatrix = [
+		Math.cos(angle_a) * domain_scale, Math.cos(angle_b) * aspectRatio * domain_scale, 0, 0,
+		Math.sin(angle_a) * domain_scale, Math.sin(angle_b) * aspectRatio * domain_scale, 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1
+	];
 
+	rangeToScreenMatrix = [
+		Math.cos(angle_c) * range_scale, Math.cos(angle_d) * aspectRatio * range_scale, 0, 0,
+		Math.sin(angle_c) * range_scale, Math.sin(angle_d) * aspectRatio * range_scale, 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1
+	];
+	
+	var axesMesh = [0.0, 0.0, domainToScreenMatrix[0], domainToScreenMatrix[1],
+					0.0, 0.0, domainToScreenMatrix[4], domainToScreenMatrix[5],
+					0.0, 0.0, rangeToScreenMatrix[0], rangeToScreenMatrix[1],
+					0.0, 0.0, rangeToScreenMatrix[4], rangeToScreenMatrix[5]];
+					
+	var axesPositionBuffer = gl.createBuffer();	// Create a buffer and put three 2d clip space points in it
+	gl.bindBuffer(gl.ARRAY_BUFFER, axesPositionBuffer);	// Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(axesMesh), gl.DYNAMIC_DRAW);
+	
+	function drawPositionsWithProgram(positionBuffer, bufferLength, program)
+	{		
+		// Bind the position buffer.
+		gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+		
+		var positionAttributeLocation = gl.getAttribLocation(program, "a_position");	// look up where the vertex data needs to go.
+		gl.enableVertexAttribArray(positionAttributeLocation);	// Turn on the attribute
+		gl.vertexAttribPointer(
+		  positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);	// Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+
+		gl.useProgram(program);	// Tell it to use our program (pair of shaders)
+
+		var domainMatLoc = gl.getUniformLocation(program, "u_dom_mat");
+		var rangeMatLoc = gl.getUniformLocation(program, "u_ran_mat");
+
+		gl.uniformMatrix4fv(domainMatLoc, false, domainToScreenMatrix);
+		gl.uniformMatrix4fv(rangeMatLoc, false, rangeToScreenMatrix);
+
+
+		// draw
+		gl.drawArrays(gl.LINES, 0, bufferLength/2);
+	}
+	
+	drawPositionsWithProgram(gridPositionBuffer, gridMesh.length, programFuncEval);
+	drawPositionsWithProgram(axesPositionBuffer, axesMesh.length, programDirect);
+	
 	if(animate)
 	{
 		requestAnimationFrame(drawFrame);
@@ -182,6 +255,11 @@ var animate = true;
 function reloadFunction()
 {
 animate = false;
+}
+
+function DrawMeshWithShader()
+{
+	
 }
 
 main();
